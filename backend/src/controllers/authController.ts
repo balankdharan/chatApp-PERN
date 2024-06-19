1b99a9d6afe21b8c4d076a45716a9a005abe4f60
balankdharan <balankdharan@gmail.com>
2024-06-19 19:07:23 +0530

    basic setup

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,5 +1,21 @@
 import { Request, Response } from "express";
+import prisma from "../db/prisma.js";
 
+export const signup = async (req: Request, res: Response) => {
+  try {
+    const { fullName, username, password, confirmPassword, gender } = req.body;
+    if (!fullName || !username || !password || !confirmPassword || !gender) {
+      return res.status(400).json({ error: "Please fill all the fields" });
+    }
+    if (password != confirmPassword) {
+      return res.status(400).json({ error: "Credientials doesn't match" });
+    }
+
+    const user = await prisma.user.findUnique({ where: { username } });
+    if (user) {
+      return res.status(400).json({ error: "Username already exists" });
+    }
+  } catch (err) {}
+};
 export const login = async (req: Request, res: Response) => {};
-export const signup = async (req: Request, res: Response) => {};
 export const logout = async (req: Request, res: Response) => {};
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,12 @@
 import express from "express";
 import authRoutes from "./routes/authRoute.js";
 import messageRoutes from "./routes/messageRoute.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
+
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
