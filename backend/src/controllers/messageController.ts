217d8377085c273f4750d1d88d217d9acc482cca
balankdharan <balankdharan@gmail.com>
2024-06-26 19:50:37 +0530

    messages

---
+++ backend/src/controllers/messageController.ts
@@ -0,0 +1,58 @@
+import { Response, Request } from "express";
+import prisma from "../db/prisma.js";
+
+//remember conversation has message field not messages
+
+export const sendMessage = async (req: Request, res: Response) => {
+  try {
+    const { message } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user.id;
+
+    let conversation = await prisma.conversation.findFirst({
+      where: {
+        participantIds: {
+          hasEvery: [senderId, receiverId],
+        },
+      },
+    });
+
+    if (!conversation) {
+      conversation = await prisma.conversation.create({
+        data: {
+          participantIds: {
+            set: [senderId, receiverId],
+          },
+        },
+      });
+    }
+
+    const newMessage = await prisma.message.create({
+      data: {
+        senderId,
+        body: message,
+        conversationId: conversation.id,
+      },
+    });
+
+    if (newMessage) {
+      conversation = await prisma.conversation.update({
+        where: {
+          id: conversation.id,
+        },
+        data: {
+          messages: {
+            connect: {
+              id: newMessage.id,
+            },
+          },
+        },
+      });
+    }
+
+    res.status(201).json(newMessage);
+  } catch (err: any) {
+    console.log("err", err);
+    return res.status(500).json({ error: " Internal server error" });
+  }
+};
